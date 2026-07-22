e04768c6b24f624bbe63ba62e6027391e95e9571
Kirdragon <kirillkutergin0@gmail.com>
2026-07-22 10:52:26 +0300

    Fix infinite loop after adding a transaction

--- main.py
+++ main.py
@@ -7,27 +7,26 @@ while True:
           "3. Посмотреть операции\n"
           "4. Выйти из программы\n")
     choice_main = int(input("Действие: "))
-    
+
     if choice_main == 1:
-        while True:
-            print ("\n1 - Добавить доход\n"
-                "2 - Добавить Расход\n"
-                "3 - Выйти назад в главное меню\n")
+        print ("\n1 - Добавить доход\n"
+        "2 - Добавить Расход\n"
+        "3 - Выйти назад в главное меню\n")
+        
+        choice_1 = int(input("Выбор: "))
+        while choice_1 not in (1,2,3):
+            print("\nВыберите только числа от 1 до 3!\n")
             choice_1 = int(input("Выбор: "))
-            if choice_1 == 1:
-                amount = float(input("Введите сумму: "))
-                manager.income(amount)
+        if choice_1 == 1:
+            amount = float(input("Введите сумму: "))
+            manager.income(amount)
             
-            elif choice_1 == 2:
-                amount = float(input("Введите сумму: "))
-                manager.expense(amount)
+        elif choice_1 == 2:
+            amount = float(input("Введите сумму: "))
+            manager.expense(amount)
             
-            elif choice_1 == 3:
-                break
-            
-            else:
-                print("\nВыберите только числа от 1 до 3!")
-                pass
+        elif choice_1 == 3:
+            pass
             
     elif choice_main == 2:
         choice_2 = int(input("Укажите id операции: "))
