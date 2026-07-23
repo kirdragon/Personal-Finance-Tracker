0369451e363427c567482304f251d57a9d6a75c0
Kirdragon <kirillkutergin0@gmail.com>
2026-07-23 19:32:35 +0300

    Add ability to update existing transactions

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from finance_manager import Manager
 manager = Manager()
+def clear_show():
+    transactions = manager.show()
+    print("Список операций: \n")
+    for transaction in transactions:
+        print (f"id - {transaction[0]} | type - {transaction[1]} | amount - {transaction[2]}")
 
 print("\nДобро пожаловать в трекер задач!")
 while True:
     print("\n1. Добавить операцию\n"
           "2. Удалить операцию\n"
           "3. Посмотреть операции\n"
-          "4. Выйти из программы\n")
+          "4. Изменить операцию\n"
+          "5. Выйти из программы\n")
     choice_main = int(input("Действие: "))
     print("")
     if choice_main not in (1,2,3,4):
@@ -46,10 +52,18 @@ while True:
             pass
         
     elif choice_main == 3:
-        transactions = manager.show()
-        print("Список операций: \n")
-        for transaction in transactions:
-            print (f"id - {transaction[0]} | type - {transaction[1]} | amount - {transaction[2]}")
+        clear_show()
     elif choice_main == 4:
+        id = int(input("Введите id операции: "))
+        while id == -1 or not manager.database.transaction_exists(id):
+            if id == -1:
+                break
+            clear_show()
+            id = int(input("\nОшибка! Операции с таким id не существует!\nДля изменение впишите существующий id или впишите -1 для выхода в главное меню: "))
+        type = input("Введите новый тип операции: ")
+        amount = float(input("Введите новую сумму: "))
+        manager.change_operation(id,type,amount)
+        print("Операция успешно изменена! ")    
+    elif choice_main == 5:
         print("\nВыход из программы...\n")
         break    
